c61949bcef725104fc22ce4b4f57f9f005926ef1
ShinjungOh <tlswjd9260@gmail.com>
2023-06-23 06:55:56 +0900

    chore: jest, testing library 설정 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { useState } from 'react';
 import reactLogo from './assets/react.svg';
-import viteLogo from '../../../../../vite.svg';
 import './App.css';
 
 function App() {
@@ -10,17 +9,22 @@ function App() {
     <>
       <div>
         <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+          <img className="logo" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank" rel="noreferrer">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Vite + React1</h1>
       <div className="card">
         <button type="button" onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
+        {count > 0 ? (
+          <p>
+            <code>The count is now: {count}</code>
+          </p>
+        ) : null}
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>

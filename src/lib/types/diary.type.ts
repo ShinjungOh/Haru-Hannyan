9936ce6185f1e4f07074e61a0bc6b3ca66ad83e3
ShinjungOh <tlswjd9260@gmail.com>
2023-08-08 23:53:24 +0900

    feat: 일기 타입 및 이미지 에셋 추가

---
+++ src/lib/types/diary.type.ts
@@ -0,0 +1,38 @@
+export enum Feeling {
+  행복 = '행복',
+  좋음 = '좋음',
+  보통 = '보통',
+  나쁨 = '나쁨',
+  화남 = '화남',
+}
+
+export enum Emotion {
+  신나는 = '신나는',
+  편안한 = '편안한',
+  뿌듯한 = '뿌듯한',
+  기대되는 = '기대되는',
+  행복한 = '행복한',
+  의욕적인 = '의욕적인',
+  설레는 = '설레는',
+  상쾌한 = '상쾌한',
+  우울한 = '우울한',
+  외로운 = '외로운',
+  불안한 = '불안한',
+  슬픈 = '슬픈',
+  화난 = '화난',
+  부담되는 = '부담되는',
+  짜증나는 = '짜증나는',
+  피곤한 = '피곤한',
+}
+
+export type Diary = {
+  diaryId?: number;
+  feel: Feeling;
+  emotions: Emotion[];
+  text: string;
+  createDate: {
+    year: number;
+    month: number;
+    date: number;
+  };
+};

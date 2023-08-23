364d4f2c151d5dfd02f2c2248752c9b86bfa5c79
ShinjungOh <tlswjd9260@gmail.com>
2023-08-24 03:42:10 +0900

    feat: 타임라인 페이지 월간 일기 조회 및 일기 삭제 기능 구현

--- src/pages/TimelinePage.tsx
+++ src/pages/TimelinePage.tsx
@@ -3,147 +3,115 @@ import Body from '@ui/components/layout/Body';
 import Menu from '@ui/components/layout/Menu';
 import styled from '@emotion/styled';
 import styleTokenCss from '@ui/styles/styleToken.css';
+import { useNavigate } from 'react-router';
+import useDateStore from '@lib/store/useDateStore';
+import { Diary } from '@lib/types/diary.type';
+import { useEffect, useState } from 'react';
+import { calendarImageTypeSrc } from '@lib/const/ImageSrc';
+import { handleAxiosError, http } from '../api/http';
+import { dayName } from './CalendarPage';
 
 export default function TimelinePage() {
+  const navigate = useNavigate();
+  const [currentDate, targetDate, setTargetDate] = useDateStore((state) => [
+    state.currentDate,
+    state.targetDate,
+    state.setTargetDate,
+  ]);
+
+  const [diary, setDiary] = useState<Diary[]>();
+
+  const handlePageToEditDiary = (diaryId: number | undefined) => {
+    navigate(`/calendar/edit?diaryId=${diaryId}`);
+  };
+
+  const handleClickDeleteDiary = async (diaryId: number | undefined) => {
+    try {
+      const isConfirm = confirm('일기를 삭제하시겠습니까?');
+      if (isConfirm) {
+        const response = await http.delete(`/diary/${diaryId}`);
+        console.log(response);
+        location.reload();
+      }
+    } catch (e) {
+      const error = handleAxiosError(e);
+      alert(error.msg);
+    }
+  };
+
+  useEffect(() => {
+    const getMonthlyDiary = async () => {
+      try {
+        if (targetDate !== null) {
+          const year = targetDate.getFullYear();
+          const month = targetDate.getMonth() + 1;
+          const response = await http.get<{ diary: Diary[] }>(`/diary?year=${year}&month=${month}`);
+          const diaryData = response.data;
+          if (diaryData && diaryData.diary) {
+            console.log(diaryData.diary);
+            setDiary(diaryData.diary);
+          }
+        }
+      } catch (e) {
+        const error = handleAxiosError(e);
+        alert(error.msg);
+      }
+    };
+
+    const setCurrentDateToTargetDate = () => {
+      const year = currentDate.getFullYear();
+      const month = currentDate.getMonth() + 1;
+      setTargetDate(year, month);
+    };
+
+    if (targetDate !== null) {
+      getMonthlyDiary();
+    } else {
+      setCurrentDateToTargetDate();
+    }
+  }, [currentDate, setTargetDate, targetDate]);
+
   return (
     <>
       <Header />
       <Container>
-        <DiaryContainer>
-          <FeelingAndDateContainer>
-            <FeelingCat>
-              <img src="images/icon/calendar/feeling-cat-great.svg" alt="feel" />
-            </FeelingCat>
-            <Date>01</Date>
-            <DayName>월요일</DayName>
-          </FeelingAndDateContainer>
-          <EmotionAndTextContainer>
-            <EmotionItem>
-              <EmotionHeader>
-                <img src="/images/icon/emotion/편안한.svg" alt="emotion" />
-              </EmotionHeader>
-              <EmotionHeader>
-                <img src="/images/icon/emotion/신나는.svg" alt="emotion" />
-              </EmotionHeader>
-              <EmotionHeader>
-                <img src="/images/icon/emotion/뿌듯한.svg" alt="emotion" />
-              </EmotionHeader>
-              <EmotionHeader>
-                <img src="/images/icon/emotion/행복한.svg" alt="emotion" />
-              </EmotionHeader>
-            </EmotionItem>
-            <TextContainer>text</TextContainer>
-          </EmotionAndTextContainer>
-        </DiaryContainer>
-        <DeleteButton>삭제</DeleteButton>
-        <DiaryContainer>
-          <FeelingAndDateContainer>
-            <FeelingCat>
-              <img src="images/icon/calendar/feeling-cat-great.svg" alt="feel" />
-            </FeelingCat>
-            <Date>01</Date>
-            <DayName>월요일</DayName>
-          </FeelingAndDateContainer>
-          <EmotionAndTextContainer>
-            <EmotionItem>
-              <EmotionHeader>
-                <img src="/images/icon/emotion/편안한.svg" alt="emotion" />
-              </EmotionHeader>
-              <EmotionHeader>
-                <img src="/images/icon/emotion/신나는.svg" alt="emotion" />
-              </EmotionHeader>
-              <EmotionHeader>
-                <img src="/images/icon/emotion/뿌듯한.svg" alt="emotion" />
-              </EmotionHeader>
-              <EmotionHeader>
-                <img src="/images/icon/emotion/행복한.svg" alt="emotion" />
-              </EmotionHeader>
-            </EmotionItem>
-            <TextContainer>text</TextContainer>
-          </EmotionAndTextContainer>
-        </DiaryContainer>
-        <DeleteButton>삭제</DeleteButton>
-        <DiaryContainer>
-          <FeelingAndDateContainer>
-            <FeelingCat>
-              <img src="images/icon/calendar/feeling-cat-great.svg" alt="feel" />
-            </FeelingCat>
-            <Date>01</Date>
-            <DayName>월요일</DayName>
-          </FeelingAndDateContainer>
-          <EmotionAndTextContainer>
-            <EmotionItem>
-              <EmotionHeader>
-                <img src="/images/icon/emotion/편안한.svg" alt="emotion" />
-              </EmotionHeader>
-              <EmotionHeader>
-                <img src="/images/icon/emotion/신나는.svg" alt="emotion" />
-              </EmotionHeader>
-              <EmotionHeader>
-                <img src="/images/icon/emotion/뿌듯한.svg" alt="emotion" />
-              </EmotionHeader>
-              <EmotionHeader>
-                <img src="/images/icon/emotion/행복한.svg" alt="emotion" />
-              </EmotionHeader>
-            </EmotionItem>
-            <TextContainer>text</TextContainer>
-          </EmotionAndTextContainer>
-        </DiaryContainer>
-        <DeleteButton>삭제</DeleteButton>
-        <DiaryContainer>
-          <FeelingAndDateContainer>
-            <FeelingCat>
-              <img src="images/icon/calendar/feeling-cat-great.svg" alt="feel" />
-            </FeelingCat>
-            <Date>01</Date>
-            <DayName>월요일</DayName>
-          </FeelingAndDateContainer>
-          <EmotionAndTextContainer>
-            <EmotionItem>
-              <EmotionHeader>
-                <img src="/images/icon/emotion/편안한.svg" alt="emotion" />
-              </EmotionHeader>
-              <EmotionHeader>
-                <img src="/images/icon/emotion/신나는.svg" alt="emotion" />
-              </EmotionHeader>
-              <EmotionHeader>
-                <img src="/images/icon/emotion/뿌듯한.svg" alt="emotion" />
-              </EmotionHeader>
-              <EmotionHeader>
-                <img src="/images/icon/emotion/행복한.svg" alt="emotion" />
-              </EmotionHeader>
-            </EmotionItem>
-            <TextContainer>text</TextContainer>
-          </EmotionAndTextContainer>
-        </DiaryContainer>
-        <DeleteButton>삭제</DeleteButton>
-        <DiaryContainer>
-          <FeelingAndDateContainer>
-            <FeelingCat>
-              <img src="images/icon/calendar/feeling-cat-great.svg" alt="feel" />
-            </FeelingCat>
-            <Date>01</Date>
-            <DayName>월요일</DayName>
-          </FeelingAndDateContainer>
-          <EmotionAndTextContainer>
-            <EmotionItem>
-              <EmotionHeader>
-                <img src="/images/icon/emotion/편안한.svg" alt="emotion" />
-              </EmotionHeader>
-              <EmotionHeader>
-                <img src="/images/icon/emotion/신나는.svg" alt="emotion" />
-              </EmotionHeader>
-              <EmotionHeader>
-                <img src="/images/icon/emotion/뿌듯한.svg" alt="emotion" />
-              </EmotionHeader>
-              <EmotionHeader>
-                <img src="/images/icon/emotion/행복한.svg" alt="emotion" />
-              </EmotionHeader>
-            </EmotionItem>
-            <TextContainer>text</TextContainer>
-          </EmotionAndTextContainer>
-        </DiaryContainer>
-        <DeleteButton>삭제</DeleteButton>
+        {diary &&
+          diary.map((el) => {
+            const date2Digit = el.createDate.date < 10 ? `0${el.createDate.date}` : el.createDate.date;
+            const getDayOfTargetDate = new Date(
+              el.createDate.year,
+              el.createDate.month - 1,
+              el.createDate.date,
+            ).getDay();
+            const dayOfWeek = dayName[getDayOfTargetDate];
+            return (
+              <>
+                <DiaryContainer onClick={() => handlePageToEditDiary(el.diaryId)}>
+                  <FeelingAndDateContainer>
+                    <FeelingCat>
+                      <img src={calendarImageTypeSrc[el.feel]} alt={el.feel} />
+                    </FeelingCat>
+                    <DiaryDate>{date2Digit}</DiaryDate>
+                    <DayName>{dayOfWeek}요일</DayName>
+                  </FeelingAndDateContainer>
+                  <EmotionAndTextContainer>
+                    <EmotionItem>
+                      <>
+                        {el.emotions &&
+                          el.emotions.map((emotion) => (
+                            <EmotionHeader>
+                              <img src={`/images/icon/emotion/${emotion}.svg`} alt={emotion} />
+                            </EmotionHeader>
+                          ))}
+                      </>
+                    </EmotionItem>
+                    <TextContainer>{el.text}</TextContainer>
+                  </EmotionAndTextContainer>
+                </DiaryContainer>
+                <DeleteButton onClick={() => handleClickDeleteDiary(el.diaryId)}>삭제</DeleteButton>
+              </>
+            );
+          })}
       </Container>
       <Menu />
     </>
@@ -163,10 +131,11 @@ const DiaryContainer = styled.div`
   padding: 18px;
   height: auto;
   min-height: 120px;
-  border-radius: 15px;
   background-color: white;
+  border-radius: 15px;
   border: 1px solid ${styleTokenCss.color.gray5};
   font-size: 14px;
+  cursor: pointer;
 `;
 
 const FeelingAndDateContainer = styled.div`
@@ -175,6 +144,7 @@ const FeelingAndDateContainer = styled.div`
   justify-content: center;
   align-items: center;
   height: auto;
+  min-height: 100px;
 `;
 
 const FeelingCat = styled.div`
@@ -191,7 +161,7 @@ const FeelingCat = styled.div`
   }
 `;
 
-const Date = styled.div`
+const DiaryDate = styled.div`
   margin-top: 8px;
   font-size: 20px;
   font-weight: 600;
@@ -220,7 +190,8 @@ const EmotionAndTextContainer = styled.div`
   justify-content: flex-start;
   align-items: center;
   width: 250px;
-  height: 100%;
+  height: auto;
+  min-height: 100px;
 `;
 
 const EmotionItem = styled.div`
@@ -256,9 +227,10 @@ const TextContainer = styled.div`
   align-items: center;
   width: 100%;
   height: auto;
-  padding: 0 10px;
+  padding: 0 5px;
   margin-top: 15px;
   font-size: 14px;
+  line-height: 1.3;
   color: ${styleTokenCss.color.gray2};
 `;
 

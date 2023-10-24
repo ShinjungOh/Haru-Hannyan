b9e408c505a795330d6076a77b177154e7b59dd7
ShinjungOh <tlswjd9260@gmail.com>
2023-10-25 03:22:13 +0900

    refactor: 설정 메뉴 컴포넌트 분리

--- src/pages/SettingPage.tsx
+++ src/pages/SettingPage.tsx
@@ -4,40 +4,63 @@ import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router';
 import { Body } from '@ui/components/layout';
 import { Menu } from '@ui/components/menu';
+import { SettingMenuList } from '@ui/components/setting';
 import { BaseButton, Typography } from '@ui/components/common';
 import { PATH } from '@lib/const/path';
 import { useAxiosErrorAlert, useConfirm } from '@lib/hooks';
 import { EMAIL_KEY, USER_NAME_KEY } from '@lib/const/localstorage';
 import { apiGetRecord } from '../api/setting';
 
-type Record = {
+export type Record = {
   diaries: number;
   answers: number;
 };
-
-const storeName = localStorage?.getItem(USER_NAME_KEY);
-const storeEmail = localStorage?.getItem(EMAIL_KEY);
 
 export function SettingPage() {
   const navigate = useNavigate();
   const confirm = useConfirm();
   const axiosErrorAlert = useAxiosErrorAlert();
 
+  const storeName = localStorage?.getItem(USER_NAME_KEY);
+  const storeEmail = localStorage?.getItem(EMAIL_KEY);
+
   const [record, setRecord] = useState<Record>({
     diaries: 0,
     answers: 0,
   });
 
-  const handleChangeUserName = () => {
-    navigate(PATH.SETTING_MODIFY_NAME);
-  };
-
-  const handleChangePassword = () => {
-    navigate(PATH.SETTING_MODIFY_PASSWORD);
-  };
-
-  const handlePagePrivacyPolicy = () => {
-    navigate(PATH.SETTING_PRIVACY);
+  const settingMenus = {
+    profile: [
+      {
+        title: '닉네임 변경하기',
+        link: PATH.SETTING_MODIFY_NAME,
+      },
+      {
+        title: '비밀번호 변경하기',
+        link: PATH.SETTING_MODIFY_PASSWORD,
+      },
+    ],
+    record: [
+      {
+        title: '총 일기 갯수',
+        description: `${record.diaries}개`,
+      },
+      {
+        title: '총 검사 갯수',
+        description: `${record.answers}개`,
+      },
+    ],
+    etc: [
+      {
+        title: '하루한냥 개발 과정이 궁금하다면?',
+        link: 'https://shinjungoh.tistory.com/category/%ED%94%84%EB%A1%9C%EC%A0%9D%ED%8A%B8/%ED%95%98%EB%A3%A8%ED%95%9C%EB%83%A5',
+        isOpen: true,
+      },
+      {
+        title: '개인정보처리방침',
+        link: PATH.SETTING_PRIVACY,
+      },
+    ],
   };
 
   const handleClickLogout = async () => {
@@ -46,6 +69,7 @@ export function SettingPage() {
       title: '로그아웃 하시겠습니까?',
       description: '하루한냥 홈으로 이동합니다.',
     });
+
     if (responseConfirm) {
       localStorage.clear();
       navigate(PATH.HOME, { replace: true });
@@ -91,61 +115,9 @@ export function SettingPage() {
         </ProfileDetail>
       </ProfileContainer>
       <Container>
-        <SettingContainer>
-          <Typography variant="body3" color={styleToken.color.gray2} style={{ marginBottom: 6, paddingLeft: 8 }}>
-            프로필
-          </Typography>
-          <InfoContainer>
-            <SettingButton type="button" onClick={handleChangeUserName}>
-              <Typography variant="body3">닉네임 변경하기</Typography>
-            </SettingButton>
-            <SettingButton type="button" onClick={handleChangePassword}>
-              <Typography variant="body3">비밀번호 변경하기</Typography>
-            </SettingButton>
-          </InfoContainer>
-        </SettingContainer>
-        <SettingContainer>
-          <Typography variant="body3" color={styleToken.color.gray2} style={{ marginBottom: 6, paddingLeft: 8 }}>
-            기록
-          </Typography>
-          <InfoContainer>
-            <SettingButton type="button">
-              <Typography variant="body3">총 일기 갯수</Typography>
-              <Typography variant="body3" fontWeight={600} color={styleToken.color.secondaryActive}>
-                {record.diaries}개
-              </Typography>
-            </SettingButton>
-            <SettingButton type="button">
-              <Typography variant="body3">총 검사 갯수</Typography>
-              <Typography variant="body3" fontWeight={600} color={styleToken.color.secondaryActive}>
-                {record.answers}개
-              </Typography>
-            </SettingButton>
-          </InfoContainer>
-        </SettingContainer>
-        <SettingContainer>
-          <Typography variant="body3" color={styleToken.color.gray2} style={{ marginBottom: 6, paddingLeft: 8 }}>
-            기타
-          </Typography>
-          <InfoContainer>
-            <SettingButton type="button">
-              <Typography variant="body3">
-                <a
-                  href="https://shinjungoh.tistory.com/category/%ED%94%84%EB%A1%9C%EC%A0%9D%ED%8A%B8/%ED%95%98%EB%A3%A8%ED%95%9C%EB%83%A5"
-                  style={{
-                    color: styleToken.color.gray2,
-                    textDecoration: 'none',
-                  }}
-                >
-                  하루한냥 개발 과정이 궁금하다면?
-                </a>
-              </Typography>
-            </SettingButton>
-            <SettingButton type="button" onClick={handlePagePrivacyPolicy}>
-              <Typography variant="body3">개인정보처리방침</Typography>
-            </SettingButton>
-          </InfoContainer>
-        </SettingContainer>
+        <SettingMenuList label="프로필" menuItems={settingMenus.profile} />
+        <SettingMenuList label="기록" menuItems={settingMenus.record} />
+        <SettingMenuList label="기타" menuItems={settingMenus.etc} />
         <BaseButton colorTheme="primary" height="68px" minHeight="68px" onClick={handleClickLogout}>
           로그아웃
         </BaseButton>
@@ -187,7 +159,6 @@ const ProfileIcon = styled.div`
   width: 80px;
   height: 80px;
   border-radius: 50%;
-  // border: 1px solid ${styleToken.color.gray4};
   background-color: ${styleToken.color.primary}75;
 
   img {
@@ -211,43 +182,4 @@ const Container = styled(Body)`
   align-items: center;
   overflow-y: auto;
   gap: 20px;
-`;
-
-const SettingContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: flex-start;
-  width: 100%;
-  height: auto;
-`;
-
-const InfoContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: flex-start;
-  text-align: center;
-  padding: 10px;
-  width: 100%;
-  height: auto;
-  margin-bottom: 6px;
-  background-color: white;
-  border-radius: 15px;
-  border: 1px solid ${styleToken.color.gray5};
-  gap: 6px;
-`;
-
-const SettingButton = styled.button`
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-  align-items: center;
-  text-align: left;
-  width: 100%;
-  height: auto;
-  padding: 14px 14px;
-  background-color: white;
-  border: unset;
-  cursor: pointer;
 `;
---
+++ src/ui/components/setting/index.ts
@@ -0,0 +1,2 @@
+export * from './SettingMenuItem';
+export * from './SettingMenuList';

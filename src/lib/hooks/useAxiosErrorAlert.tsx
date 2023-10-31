2cbcca305ab69114ce7c98e1aeb11b148d23537a
ShinjungOh <tlswjd9260@gmail.com>
2023-10-31 15:00:41 +0900

    fix: 유효하지 않은 토큰 처리 이슈 해결

--- src/lib/hooks/useAxiosErrorAlert.tsx
+++ src/lib/hooks/useAxiosErrorAlert.tsx
@@ -1,15 +1,23 @@
+import { useNavigate } from 'react-router';
 import { useAlert } from '@lib/hooks/useAlert';
+import { PATH } from '@lib/const/path';
 import { handleAxiosError } from '../../api/http';
 
 export function useAxiosErrorAlert() {
+  const navigate = useNavigate();
   const alert = useAlert();
 
   const axiosErrorAlert = async (e: any) => {
     const error = handleAxiosError(e);
-    await alert({
+    const responseAlert = await alert({
       type: 'danger',
       title: error.msg,
     });
+
+    if (error.msg === '올바른 접근이 아닙니다.' && responseAlert) {
+      localStorage.clear();
+      navigate(PATH.HOME, { replace: true });
+    }
   };
 
   return axiosErrorAlert;

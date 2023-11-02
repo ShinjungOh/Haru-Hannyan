99004bab9832cecd1254d8fe3073e5a570f163ed
ShinjungOh <tlswjd9260@gmail.com>
2023-11-02 20:43:28 +0900

    fix: 모바일 일기 작성 페이지에서 감정 선택 취소 hover 이슈 해결

--- src/ui/components/diary/EmotionContainer.tsx
+++ src/ui/components/diary/EmotionContainer.tsx
@@ -1,9 +1,9 @@
 import styled from '@emotion/styled';
-import { Emotion } from '@lib/types';
 import { styleToken } from '@ui/styles';
+import { Emotion } from '@lib/types';
+import { EmotionItem } from '@ui/components/diary/EmotionItem';
 import { Typography } from '@ui/components/common';
 import { EMOTION_TYPE } from '@lib/const/imageSrc';
-import { EmotionItem } from '@ui/components/diary/EmotionItem';
 
 type EmotionContainerProps = {
   diary: any;
@@ -20,13 +20,14 @@ export function EmotionContainer({ diary, onClick }: EmotionContainerProps) {
         <>
           {EMOTION_TYPE.map((el) => {
             const isSelected = diary.emotions.includes(el.emotion);
+
             return (
               <EmotionItem
                 key={el.emotion}
                 emotion={el.emotion}
                 imgSrc={el.url}
-                onClick={onClick}
                 isSelected={isSelected}
+                onClick={onClick}
               />
             );
           })}
